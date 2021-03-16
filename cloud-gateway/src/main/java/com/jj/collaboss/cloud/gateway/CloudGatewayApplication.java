de99af8c6762a7477b10485b6f0f281811abbb02
deluxjun@gmail.com <s9612088>
2021-03-15 20:31:27 -0400

    eureka server & cloud gateway

--- cloud-gateway/src/main/java/com/jj/collaboss/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/jj/collaboss/cloud/gateway/CloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.jj.collaboss.authserver;
+package com.jj.collaboss.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class AuthserverApplication {
+public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthserverApplication.class, args);
+		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
 }
--- portal/src/main/java/com/jj/collaboss/SbpApplication.java
+++ portal/src/main/java/com/jj/collaboss/SbpApplication.java
@@ -5,9 +5,11 @@ import lombok.Setter;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SbpApplication {
 
     public static void main(String[] args) {
